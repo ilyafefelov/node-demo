0f964ee8ad1cb0206f305fe802966ca6ffc04c4b
full_iron <ilyafefelov@gmail.com>
2022-05-24 18:28:59 +0000

    serve html files at "/"

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
+console.log("Hello World")
 let express = require('express');
 let app = express();
-console.log("Hello World")
-
-
+app.get('/',(req, res) => {
+  // res.send('Hello Express');
+  // absolutePath = __dirname + relativePath/file.ext
+  console.log(__dirname + '/views/index.html')
+  // res.senfFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + "/views/index.html");
+  
+})
 
 
 
