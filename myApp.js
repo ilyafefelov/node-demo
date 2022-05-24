37b5553093438f5bf6a40bcb378ac93901efec22
full_iron <ilyafefelov@gmail.com>
2022-05-24 18:45:39 +0000

    serving static files dir

--- myApp.js
+++ myApp.js
@@ -4,11 +4,13 @@ let app = express();
 app.get('/',(req, res) => {
   // res.send('Hello Express');
   // absolutePath = __dirname + relativePath/file.ext
-  console.log(__dirname + '/views/index.html')
+  // console.log(__dirname + '/views/index.html')
   // res.senfFile(__dirname + '/views/index.html')
   res.sendFile(__dirname + "/views/index.html");
   
 })
+
+app.use('/public', express.static(__dirname + "/public"))
 
 
 
