289bc4c9a9653df7d3eb53702b769db9ec4a0997
full_iron <ilyafefelov@gmail.com>
2022-05-24 19:11:08 +0000

    environment

--- myApp.js
+++ myApp.js
@@ -1,19 +1,21 @@
 console.log("Hello World")
 let express = require('express');
 let app = express();
+
 app.get('/',(req, res) => {
   // res.send('Hello Express');
   // absolutePath = __dirname + relativePath/file.ext
-  // console.log(__dirname + '/views/index.html')
   // res.senfFile(__dirname + '/views/index.html')
   res.sendFile(__dirname + "/views/index.html");
   
 })
-
-app.use('/public', express.static(__dirname + "/public"))
-
-
-
+.use('/public', express.static(__dirname + "/public"))
+  
+.get('/json', (req,res) => {
+  res.json(
+    {"message": process.env.MESSAGE_STYLE == 'uppercase' ? "HELLO JSON":"Hello json"}
+  )
+})
 
 
 
